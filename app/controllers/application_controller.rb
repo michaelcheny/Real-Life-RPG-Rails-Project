c7c5bc323bd53a6a2296afd690dd974e0fe7f7fc
Michael Chen <michaelchengaming@gmail.com>
2019-10-07 16:01:30 -0700

    add current_user, logged_in?, and authenticate methods

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,24 @@
 class ApplicationController < ActionController::Base
+
+  private
+
+  def current_user
+    # return unless session[:user_id]
+    User.find_by(id: session[:user_id])
+  end
+
+  def logged_in?
+    !current_user.nil?
+  end
+
+  def authenticate
+    if !logged_in? || current_user.nil?
+      redirect_to login_path
+    end
+  end
+
+  # checks if user is logged in and available
+  def authorize?
+    !current_user.nil? && !!logged_in?
+  end
 end

4059de4290861742ae2e61ecd3bf6ad14d3a3ab5
Michael Chen <michaelchengaming@gmail.com>
2019-10-07 15:58:55 -0700

    user has secure password and can create user accounts, have flash messages

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,48 @@
 class UsersController < ApplicationController
+  
+  before_action :find_user, only: [:index, :show, :edit, :update, :destroy, :index]
+
+  def index
+
+  end
+
+  def show
+
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params(:username, :password, :password_confirmation))
+    if @user.save
+      # to log in
+      session[:user_id] = @user.id
+      flash[:notice] = "Yo #{@user.username}!"
+      redirect_to dashboard_path
+    else
+      flash[:alert] = "WTF BRO"
+      render "new"
+    end
+  end
+
+  def update
+
+  end
+
+  def destroy
+
+  end
+
+  private
+
+  def user_params(*args)
+    params.require(:user).permit(*args)
+  end
+
+  def find_user
+    @user = User.find(params[:id])
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,11 @@
 Rails.application.routes.draw do
   root 'welcome#index'
 
+  get '/signup' => 'user#new', as: 'signup'
+  get '/login' => 'sessions#new', as: 'login'
+  post '/login' => 'sessions#create'
+  delete '/logout' => 'sessions#destroy'
+
   resources :users
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end

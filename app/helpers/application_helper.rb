8942510f0b2c107cd98a4fca05163bc0a10c6e44
Michael Chen <michaelchengaming@gmail.com>
2019-10-07 15:49:08 -0700

    create navbar for logged out and logged in users

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,12 @@
 module ApplicationHelper
+
+  # Nav bar for log in and log out
+  def navbar
+    if logged_in?
+      render 'layouts/logged_in_navbar'
+    else
+      render 'layouts/logged_out_navbar'
+    end
+  end
+
 end

b1e49f3e8063682e11232dcb0d8d02d5a9286e6b
Michael Chen <michaelchengaming@gmail.com>
2019-10-12 23:49:09 -0700

    begin set up of omniauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -15,6 +15,24 @@ class SessionsController < ApplicationController
     end
   end
 
+
+  def googleAuth
+    # Get access tokens from the google server
+    access_token = request.env["omniauth.auth"]
+    @user = User.from_omniauth(access_token)
+    
+    # Access_token is used to authenticate request made from the rails application to the google server
+    @user.google_token = access_token.credentials.token
+    # Refresh_token to request new access_token
+    # Note: Refresh_token is only sent once during the first request
+    refresh_token = access_token.credentials.refresh_token
+    @user.google_refresh_token = refresh_token if refresh_token.present?
+    @user.save
+    log_in(@user)
+    redirect_to dashboard_path
+  end
+
+
   def destroy
     session.delete(:user_id)
     redirect_to root_path
--- app/models/user.rb
+++ app/models/user.rb
@@ -17,4 +17,13 @@ class User < ApplicationRecord
   validates :password_confirmation, presence: true, on: :create
   validates :password, presence: true, length: { in: 6..20 }, confirmation: true, unless: ->(u){ u.password.blank? }
 
+
+  def self.from_omniauth(auth)
+    # Creates a new user only if it doesn't exist
+    where(email: auth.info.email).first_or_initialize do |user|
+      user.name = auth.info.name
+      user.email = auth.info.email
+    end
+  end
+
 end

505a6f339dfeae23d3798d2c414ad9cd88cebb8b
Michael Chen <michaelchengaming@gmail.com>
2019-10-19 16:53:55 -0700

    create method to calculate levels away from quest requirement and add comments for easy reading

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,29 +1,30 @@
 module UsersHelper
 
 
-  # def populate_skills_if_empty(user)
-  #   if user.skills.empty?
-  #     Skill.all.each do |s|
-  #       user.skills << s
-  #     end
-  #   end
-  # end
-
-
+  # sorts the total exp in decending order for the high score's list.
   def sort_by_total_exp(users)
     users.sort_by{|u| u.total_exp_pts}.reverse
   end
 
 
+  # Returns the how far away the user's total level is from the quest requirements.
+  def levels_away_from_req(user, quest)
+    difference = quest.level_requirement - user.total_level
+    return difference.to_s
+  end
+
+
+  # Returns true or false based on whether the user meets the quest requirements.
   def user_meets_requirements(user, quest)
     if user.total_level >= quest.level_requirement
       true
     else
       false
-      flash[:error] = "You do not meet the requirements."
+      flash[:error] = "You are " + levels_away_from_req(user, quest) + " levels are from the requirements."
       redirect_to quests_path
       return
     end
   end
+  
   
 end

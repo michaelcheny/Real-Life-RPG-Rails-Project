e2f4aa7404e4bc3ccd6e56289f0401619e59958d
Michael Chen <michaelchengaming@gmail.com>
2019-10-18 19:58:58 -0700

    create quest helper method to render the footer depending on the condition of the quest, sort quest by newest on top

--- app/helpers/quests_helper.rb
+++ app/helpers/quests_helper.rb
@@ -1,2 +1,15 @@
 module QuestsHelper
+
+  def quest_footer_for_quest(quest)
+    if params[:user_id]
+      "render add button to complete task here"
+    else
+      if current_user.quests.include?(quest)
+        render 'quests/already_added_button'
+      else
+        render 'quests/accept_quest_button', quest: quest
+      end
+    end
+  end
+
 end
